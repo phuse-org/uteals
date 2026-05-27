225cec520d17b45a4653e4ca8f19ebbeed5c6f4b
Konrad Pagacz <konrad.pagacz@gmail.com>
2026-05-27 09:25:38 +0200

    prefix package functions

--- tests/testthat/test-title_footer_decorator.R
+++ tests/testthat/test-title_footer_decorator.R
@@ -1,17 +1,17 @@
 titles_file <- testthat::test_path("dps_titles_08042026.xlsx")
 
-test_that("title_footer_decorator works when choices and selected are NULL", {
+testthat::test_that("title_footer_decorator works when choices and selected are NULL", {
   result <- title_footer_decorator("plot", titles_file, choices = NULL, selected = NULL)
-  expect_s3_class(result, "teal_data_module")
+  testthat::expect_s3_class(result, "teal_data_module")
 })
 
-test_that("title_footer_decorator works when selected is in choices", {
+testthat::test_that("title_footer_decorator works when selected is in choices", {
   result <- title_footer_decorator("plot", titles_file, choices = c("TSFLAB01", "TSFLAB01b"), selected = "TSFLAB01")
-  expect_s3_class(result, "teal_data_module")
+  testthat::expect_s3_class(result, "teal_data_module")
 })
 
-test_that("title_footer_decorator errors when selected is not in choices", {
-  expect_error(
+testthat::test_that("title_footer_decorator errors when selected is not in choices", {
+  testthat::expect_error(
     title_footer_decorator("plot", titles_file, choices = c("TSFLAB01", "TSFLAB01b"), selected = "TBL99")
   )
 })
