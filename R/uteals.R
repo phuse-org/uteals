96c7d08b62b13b0d203f5d93e2438f75e13bb3ed
Konrad Pagacz <konrad.pagacz@gmail.com>
2026-06-30 15:35:12 +0200

    fix checks

--- R/uteals.R
+++ R/uteals.R
@@ -2,4 +2,7 @@
 #'
 #' Provides decorator and transformator modules
 #' for `teal` modules.
+#' @importFrom jsonlite base64_enc read_json
+#' @importFrom teal.reporter Reporter
+#' @importFrom zip zip
 "_PACKAGE"
